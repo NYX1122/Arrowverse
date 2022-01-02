9586f971b9ed21f1daa10d9a387da73acfce8cc7
BoDee <nyx0hemir@gmail.com>
2022-01-02 16:46:20 -0700

    finished header functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import './App.css';
 import theme from './theme.js';
@@ -15,7 +15,6 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Header></Header>
-        <Link to='/Cast'>Cast</Link>
       </ThemeProvider>
       <Outlet/>
     </Box>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,39 @@
 import React from 'react';
-import { Box, Typography, AppBar, Toolbar, IconButton } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+import { Box, Typography, AppBar, Toolbar, IconButton, Menu, MenuItem } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 
 function Header() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
                     <Typography sx={{ variant: 'h1', display: 'flex', fontWeight: 'bold', fontSize: 40, textAlign: 'left', flexGrow: 1 }}>Arrowverse</Typography>
-                    <IconButton size="large" color="inherit" aria-label="menu">
+                    <IconButton size="large" color="inherit" id="menu-button" 
+                        aria-controls={open ? 'menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        onClick={handleClick}
+                    >
                         <MenuIcon/>
                     </IconButton>
+                    <Menu id='menu' anchorEl={anchorEl} open={open}
+                        onClose={handleClose}
+                        menulistprops={{ 'aria-labelledby': 'menu-button' }}    
+                    >
+                        <MenuItem onClick={handleClose}><Link to='/'><Typography sx={{ color: 'primary.main' }}>Show</Typography></Link></MenuItem>
+                        <MenuItem onClick={handleClose}><Link to='/Cast'><Typography sx={{ color: 'primary.main' }}>Cast</Typography></Link></MenuItem>
+                    </Menu>
                 </Toolbar>
             </AppBar>
         </Box>
