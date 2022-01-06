69342e6bf36dcd6d7666bb50a781e6315d4554f8
BoDee <nyx0hemir@gmail.com>
2022-01-05 18:50:18 -0700

    finished castPage and export functionality

--- src/components/CastCard/index.js
+++ src/components/CastCard/index.js
@@ -3,13 +3,36 @@ import React from 'react';
 import { Card, CardContent, CardMedia, Button, Typography, Container, Grid } from '@mui/material';
 
 function CastCard({ name, photo, country, birthday, gender }) {
-    console.log(name);
     return (
         <Card>
             <CardMedia component='img' height='300' image={photo} alt={name} />
-            <CardContent sx={{ backgroundColor: 'primary.main', padding: 1 }}>
-                <Typography variant='h6' sx={{ color: 'secondary.main' }}>{name}</Typography>
-                <Typography className='landingCardTitles' variant='h6' sx={{ color: 'secondary.main' }}>{birthday}</Typography>
+            <CardContent sx={{ backgroundColor: 'primary.main', padding: .8 }}>
+                <Grid container spacing={1} justifyContent='space-between'>
+                    <Grid item xs={6}>
+                        <Typography sx={{ color: 'secondary.main', fontWeight: 'bolder', fontSize: 14, textAlign: 'left' }}>{name}</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography sx={{ color: 'secondary.main', fontWeight: 'bolder', fontSize: 14, textAlign: 'right' }}>G: {gender}</Typography>
+                    </Grid>
+                    {(birthday === null) ? 
+                        <Grid item xs={12}>
+                            <Typography sx={{ color: 'secondary.main', fontWeight: 'bolder', fontSize: 13, textAlign: 'left' }}>Date of Birth: Not Found</Typography>
+                        </Grid>
+                        :
+                        <Grid item xs={12}>
+                            <Typography sx={{ color: 'secondary.main', fontWeight: 'bolder', fontSize: 13, textAlign: 'left' }}>Date of Birth: {birthday}</Typography>
+                        </Grid>
+                    }
+                    {(country === null) ? 
+                        <Grid item xs={12}>
+                            <Typography sx={{ color: 'secondary.main', fontWeight: 'bolder', fontSize: 13, textAlign: 'left' }}>Country of Birth: Not Found</Typography>
+                        </Grid>
+                        :
+                        <Grid item xs={12}>
+                            <Typography sx={{ color: 'secondary.main', fontWeight: 'bolder', fontSize: 13, textAlign: 'left' }}>Country of Birth: {country.name}</Typography>
+                        </Grid>
+                    }
+                </Grid>
             </CardContent>
         </Card>
     );
--- src/pages/CastPage.js
+++ src/pages/CastPage.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { CSVLink } from 'react-csv';
 
-import { Grid, Container } from '@mui/material';
+import { Grid, Container, Typography, Button } from '@mui/material';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 import Loader from '../components/Loader';
 import CastCard from '../components/CastCard';
+import { act } from 'react-dom/cjs/react-dom-test-utils.production.min';
 
 export default function CastPage() {
     const [castData, setData] = useState(null);
@@ -14,15 +17,66 @@ export default function CastPage() {
        .then(data => setData(data));
     }, []);
   
+    //While statement used to give fetch enough time before page is rendered.
     while (!castData) {
       return <Loader></Loader>
     }
-    console.log(castData);
+
+    //Logic used to remove duplicate entries in cast list.
+    const names = [];
+    const newCastData = [];
+    castData.map((actor, i) => {
+        names.push(actor.person.name);
+    });
+    var uniqueNames = [...new Set(names)];
+    castData.map((actor) => {
+        uniqueNames.map((name, i) => {
+            if (actor.person.name === name) {
+                newCastData.push(actor);
+                uniqueNames.splice(i, 1);
+            }
+        })
+    });
+
+    //format data for csv download package
+
+    const formattedData = [];
+    newCastData.map((actor) => {
+        formattedData.push({ name: actor.person.name, photo: actor.person.image.original, country: actor.person.country, birthday: actor.person.birthday, gender: actor.person.gender });
+    });
+
+    const headers = [
+        { label: 'Name', key: 'name' },
+        { label: 'Photo', key: 'photo' },
+        { label: 'Country of Birth', key: 'country' },
+        { label: 'Date of Birth', key: 'birthday' },
+        { label: 'Gender', key: 'gender' }
+    ];
+
+    const csvReport = {
+        data: formattedData,
+        headers: headers,
+        filename: 'ArrowCast.csv'
+    };
+    
     return (
-        <Container sx={{ marginTop: '4%' }}>
+        <Container sx={{ marginTop: 1 }}>
+            <Grid container spacing={0} justifyContent='space-between' alignItems='center' sx={{ marginBottom: 1 }}>
+                <Grid item xs={6}>
+                    <Typography sx={{ fontWeight: 'bolder', fontSize: 18 }}>Export Cast Data</Typography>
+                </Grid>
+                <Grid item xs={3}>
+                    <ArrowForwardIcon></ArrowForwardIcon>
+                    <ArrowForwardIcon></ArrowForwardIcon>
+                    <ArrowForwardIcon></ArrowForwardIcon>
+                </Grid>
+                <Grid item xs={3}>
+                    <CSVLink {...csvReport}><Button variant='outlined' sx={{ backgroundColor: 'primary.main', borderColor: 'secondary.main', fontWeight: 'bolder', color: 'secondary.main' }}>Export</Button></CSVLink>
+                </Grid>
+            </Grid>
             <Grid container spacing={1} justifyContent='space-between'>
-                {castData.map((actor) => (
-                    <Grid item xs={6}>
+                {newCastData.map((actor, i) => (
+                    <Grid item xs={6} key={i}>
                         <CastCard
                             name={actor.person.name}
                             photo={actor.person.image.original}
